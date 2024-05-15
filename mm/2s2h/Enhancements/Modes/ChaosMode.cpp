f2a892c03835a6bba7655e08f880afaee482c81f
aMannus <mannusmenting@gmail.com>
2024-05-15 20:15:20 +0200

    Most regular enemies

--- mm/2s2h/Enhancements/Modes/ChaosMode.cpp
+++ mm/2s2h/Enhancements/Modes/ChaosMode.cpp
@@ -13,32 +13,51 @@ typedef struct EnemyEntry {
 } EnemyEntry;
 
 static EnemyEntry enemySpawnTable[RANDOMIZED_ENEMY_SPAWN_TABLE_SIZE] = {
-    { ACTOR_EN_OKUTA, 0 },   // Oktorok
-    { ACTOR_EN_WALLMAS, 0 }, // Wallmaster
-    { ACTOR_EN_DODONGO, 0 }, // Dodongo
-    { ACTOR_EN_FIREFLY, 0 }, // Keese Fire
-    { ACTOR_EN_FIREFLY, 3 },  // Keese Normal
-    { ACTOR_EN_FIREFLY, 4 },  // Keese Ice
-    { ACTOR_EN_TITE, -4 },   // Tektite
-    { ACTOR_EN_PEEHAT, 0 },  // Peahat
-    { ACTOR_EN_DINOFOS, 0 }, // Dinolfos
-    { ACTOR_EN_BUBBLE, 0 },  // Shabom
-    { ACTOR_EN_ST, 0 },      // Large Skulltula
-    { ACTOR_EN_FAMOS, 0 },   // Death Armos (Inv. Stone Tower)
-    { ACTOR_EN_AM, 0 },      // Armos
-    { ACTOR_EN_DEKUBABA, 0 }, // Deku Baba
-    { ACTOR_EN_DEKUNUTS, 0 }, // Mad Scrub
-    { ACTOR_EN_BB, 0 },       // Blue Bubble
-    { ACTOR_EN_VM, 0 },       // Beamos
-    { ACTOR_EN_FLOORMAS, 0 }, // Floormaster
-    { ACTOR_EN_RD, 0 },       // Redead/Gibdo
-    { ACTOR_EN_SB, 0 },       // Shellblade
-    { ACTOR_EN_KAREBABA, 0 }, // Wilted Baba
-    { ACTOR_EN_RR, 0 },       // Like Like
-    { ACTOR_EN_IK, 1 },       // Iron Knuckle
-    { ACTOR_EN_TUBO_TRAP, 0 }, // Flying Pot Trap
-    { ACTOR_EN_FZ, 0 },        // Freezard
-    { ACTOR_EN_WF, 0 },        // Wolfos
+    { ACTOR_EN_OKUTA, 0 },      // Oktorok
+    { ACTOR_EN_WALLMAS, 0 },    // Wallmaster
+    { ACTOR_EN_DODONGO, 0 },    // Dodongo
+    { ACTOR_EN_FIREFLY, 0 },    // Keese Fire
+    { ACTOR_EN_FIREFLY, 3 },    // Keese Normal
+    { ACTOR_EN_FIREFLY, 4 },    // Keese Ice
+    { ACTOR_EN_TITE, -4 },      // Tektite
+    { ACTOR_EN_PEEHAT, 0 },     // Peahat
+    { ACTOR_EN_DINOFOS, 0 },    // Dinolfos
+    { ACTOR_EN_BUBBLE, 0 },     // Shabom
+    { ACTOR_EN_ST, 0 },         // Large Skulltula
+    { ACTOR_EN_FAMOS, 0 },      // Death Armos (Inv. Stone Tower)
+    { ACTOR_EN_AM, 0 },         // Armos
+    { ACTOR_EN_DEKUBABA, 0 },   // Deku Baba
+    { ACTOR_EN_DEKUNUTS, 0 },   // Mad Scrub
+    { ACTOR_EN_BB, 0 },         // Blue Bubble
+    { ACTOR_EN_VM, 0 },         // Beamos
+    { ACTOR_EN_FLOORMAS, 0 },   // Floormaster
+    { ACTOR_EN_RD, 0 },         // Redead/Gibdo
+    { ACTOR_EN_SB, 0 },         // Shellblade
+    { ACTOR_EN_KAREBABA, 0 },   // Wilted Baba
+    { ACTOR_EN_RR, 0 },         // Like Like
+    { ACTOR_EN_IK, 1 },         // Iron Knuckle
+    { ACTOR_EN_TUBO_TRAP, 0 },  // Flying Pot Trap
+    { ACTOR_EN_FZ, 0 },         // Freezard
+    { ACTOR_EN_WF, 0 },         // Wolfos
+    { ACTOR_EN_SKB, 0 },        // Stalchild
+    { ACTOR_EN_CROW, 0 },       // Guay
+    { ACTOR_EN_SLIME, 0 },      // Chuchu
+    { ACTOR_EN_PR, 0 },         // Desbreko
+    { ACTOR_EN_BAGUO, 0 },      // Nejiron
+    { ACTOR_EN_BAT, 0 },        // Bad Bat
+    { ACTOR_EN_MKK, 0 },        // Black Boe
+    { ACTOR_EN_RAT, 0 },        // Real Bombchu
+    { ACTOR_EN_KAME, 0 },       // Snapper
+    { ACTOR_EN_WDHAND, 0 },     // Dexihand
+    { ACTOR_EN_SNOWMAN, 0 },    // Eeno
+    { ACTOR_EN_PO_SISTERS, 0 }, // Poe Sister
+    { ACTOR_EN_PP, 0 },         // Hiploop
+    { ACTOR_EN_POH, 0 },        // Poe
+    { ACTOR_EN_BEE, 0 },        // Giant Bee
+    { ACTOR_EN_DRAGON, 0 },     // Deep Python
+    { ACTOR_EN_KAIZOKU, 0 },    // Fighter pirate
+    { ACTOR_EN_THIEFBIRD, 0 },  // Takkuri
+    { ACTOR_EN_BAL, 0 },        // Tingle with balloon
 };
 
 static uint8_t frameCounter = 0;
